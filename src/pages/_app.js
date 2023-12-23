e21d2d45d3510ececb0deba3b2271ad5418c474a
lucasptcastro <lucasptcastro@gmail.com>
2023-12-23 10:05:46 -0300

    ✨ feat: moved fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
 import "../styles/globals.css";
+import { Roboto } from "next/font/google";
+import { Baloo_2 } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["400", "900"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
+const baloo2 = Baloo_2({
+  weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  variable: "--font-baloo2",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${roboto.className} ${baloo2.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,20 +1,7 @@
 "use client";
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
-import { Baloo_2 } from "next/font/google";
 
 import Navbar from "../components/Navbar";
-
-const roboto = Roboto({
-  weight: ["400", "900"],
-  subsets: ["latin"],
-  variable: "--font-roboto",
-});
-const baloo2 = Baloo_2({
-  weight: ["400", "500", "600", "700", "800"],
-  subsets: ["latin"],
-  variable: "--font-baloo2",
-});
 
 // export const metadata: Metadata = {
 //   title: "Coffee Delivery",
@@ -27,9 +14,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className={`${baloo2.className} ${roboto.className}`}>
+    <>
       <Navbar />
       {children}
-    </div>
+    </>
   );
 }
