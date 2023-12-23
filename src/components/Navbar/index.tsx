fe951053538b5222aa4f88b10b2a4e119d74f043
lucasptcastro <lucasptcastro@gmail.com>
2023-12-22 22:26:19 -0300

    ✨ feat: created navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,20 @@
+import { MapPin } from "@phosphor-icons/react";
 import Image from "next/image";
+import Cart from "../Cart";
 
 export default function Navbar() {
   return (
-    <nav className="flex h-16 w-full flex-row justify-between bg-orange-300 px-20">
+    <nav className="flex h-20 w-full items-center justify-between bg-white px-[370px] shadow-lg">
       <Image alt="" src={"/Logo.svg"} width={84.95} height={40} />
 
-      <div></div>
+      {/* Localização e carrinho */}
+      <div className="flex items-center gap-3">
+        <div className="flex h-9 w-fit items-center gap-1 rounded-md bg-purple-light p-2 shadow-sm">
+          <MapPin size={22} weight="fill" className="text-purple" />
+          <span className="font-roboto text-purple-dark">Porto Alegre, RS</span>
+        </div>
+        <Cart />
+      </div>
     </nav>
   );
 }
