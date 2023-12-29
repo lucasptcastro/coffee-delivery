5f358fb1b46cba1c2e260bf4fe111bd06a77dc1c
lucasptcastro <lucasptcastro@gmail.com>
2023-12-29 16:36:31 -0300

    ✨ feat: added quantity to cart

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -2,22 +2,28 @@ import { ShoppingCart } from "@phosphor-icons/react";
 import { ButtonHTMLAttributes } from "react";
 
 interface ICart {
-  type?: "empty" | "full";
+  quantity?: number;
   props?: ButtonHTMLAttributes<HTMLButtonElement>;
   className?: ButtonHTMLAttributes<HTMLButtonElement>;
 }
 
-export default function Cart({ type = "empty", props, className }: ICart) {
+export default function Cart({ quantity = 0, props, className }: ICart) {
   return (
     <button
-      className={`flex h-7 w-7 items-center justify-center rounded-md bg-yellow-light p-2 shadow-sm xl:h-9 xl:w-9 ${className} transition-shadow hover:shadow-md`}
+      className={`relative flex h-7 w-7 items-center justify-center rounded-md bg-yellow-light p-2 shadow-sm md:h-9 md:w-9 ${className} transition-shadow hover:shadow-md`}
       {...props}
     >
       <ShoppingCart
         size={22}
         weight="fill"
-        className="h-4 w-4 text-yellow-dark xl:h-[22px] xl:w-[22px]"
+        className="h-4 w-4 text-yellow-dark md:h-[22px] md:w-[22px]"
       />
+
+      {quantity > 0 && (
+        <div className="absolute -right-1 -top-1 flex h-3 w-3 items-center justify-center rounded-full bg-yellow-dark font-roboto text-[10px] font-bold text-white md:-right-2 md:-top-2 md:h-5 md:w-5 md:text-sm">
+          {quantity}
+        </div>
+      )}
     </button>
   );
 }
