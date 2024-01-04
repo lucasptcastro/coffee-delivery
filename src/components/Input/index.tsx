5dd9fc8b93822721d86eeced4171abb6d0f1d748
lucasptcastro <lucasptcastro@gmail.com>
2024-01-03 22:32:33 -0300

    ✨ feat: created input text

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,30 @@
 import { Minus, Plus } from "@phosphor-icons/react";
+import { InputHTMLAttributes } from "react";
 
 interface IInput {
-  type?: "number";
+  type?: "text" | "number";
+  props?: InputHTMLAttributes<HTMLInputElement>;
+  className?: string;
 }
 
-export default function Input({ type }: IInput) {
-  return (
-    <div className="flex h-6 w-12 flex-row items-center justify-center gap-2 rounded-md bg-base-button p-1 xl:h-8 xl:w-[72px]">
-      <Minus weight="bold" className="text-xs text-purple xs:text-sm" />
-      <span className="font-roboto text-xs text-base-title xs:text-base">
-        1
-      </span>
-      <Plus weight="bold" className="text-xs text-purple xs:text-sm" />
-    </div>
-  );
+export default function Input({ type, props, className }: IInput) {
+  if (type == "text") {
+    return (
+      <input
+        className={`h-10 rounded border border-base-button bg-base-input p-3 font-roboto text-sm text-base-text ${className} outline-yellow-dark placeholder:text-base-label focus:text-base-text focus:placeholder:text-black/0`}
+        type="text"
+        {...props}
+      />
+    );
+  } else if (type == "number") {
+    return (
+      <div className="flex h-6 w-12 flex-row items-center justify-center gap-2 rounded-md bg-base-button p-1 xl:h-8 xl:w-[72px]">
+        <Minus weight="bold" className="text-xs text-purple xs:text-sm" />
+        <span className="font-roboto text-xs text-base-title xs:text-base">
+          1
+        </span>
+        <Plus weight="bold" className="text-xs text-purple xs:text-sm" />
+      </div>
+    );
+  }
 }
