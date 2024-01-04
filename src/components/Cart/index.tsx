0d1165d8662ab4a6b6529ff8350e5f9eb8049f37
lucasptcastro <lucasptcastro@gmail.com>
2024-01-03 22:32:21 -0300

    🌈 style: adjusted buttons size

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -10,17 +10,17 @@ interface ICart {
 export default function Cart({ quantity = 0, props, className }: ICart) {
   return (
     <button
-      className={`relative flex h-7 w-7 items-center justify-center rounded-md bg-yellow-light p-2 shadow-sm md:h-9 md:w-9 ${className} transition-shadow hover:shadow-md`}
+      className={`relative flex h-7 w-7 items-center justify-center rounded-md bg-yellow-light p-2 shadow-sm xs:h-8 xs:w-8 md:h-9 md:w-9 ${className} transition-shadow hover:shadow-md`}
       {...props}
     >
       <ShoppingCart
         size={22}
         weight="fill"
-        className="h-4 w-4 text-yellow-dark md:h-[22px] md:w-[22px]"
+        className="h-4 w-4 text-yellow-dark xs:h-5 xs:w-5 md:h-[22px] md:w-[22px]"
       />
 
       {quantity > 0 && (
-        <div className="absolute -right-1 -top-1 flex h-3 w-3 items-center justify-center rounded-full bg-yellow-dark font-roboto text-[10px] font-bold text-white md:-right-2 md:-top-2 md:h-5 md:w-5 md:text-sm">
+        <div className="absolute -right-1 -top-1 flex h-3 w-3 items-center justify-center rounded-full bg-yellow-dark font-roboto text-[8px] font-bold text-white xs:h-4 xs:w-4 xs:text-[10px] md:-right-2 md:-top-2 md:h-5 md:w-5 md:text-sm">
           {quantity}
         </div>
       )}
