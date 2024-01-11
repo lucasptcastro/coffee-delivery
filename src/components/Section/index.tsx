037733913467137d7d501fa36c207128600f91f6
lucasptcastro <lucasptcastro@gmail.com>
2024-01-10 21:29:36 -0300

    🌈 style: removed classes

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -4,11 +4,5 @@ interface ISection {
 }
 
 export default function Section({ children, className }: ISection) {
-  return (
-    <section
-      className={`pt-40 xl:h-[544px] xl:px-64 xl:pt-96 2xl:flex-row 2xl:gap-0 2xl:pt-32 3xl:px-[370px] ${className}`}
-    >
-      {children}
-    </section>
-  );
+  return <section className={`${className}`}>{children}</section>;
 }
