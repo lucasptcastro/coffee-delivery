340b3c00e5a22ef15e9a365e6ab14efb301cf517
lucasptcastro <lucasptcastro@gmail.com>
2024-01-10 21:27:45 -0300

    ✨ feat: created secondary button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,8 @@ interface IButton {
   label: string;
   type?: "primary" | "secundary";
   props?: ButtonHTMLAttributes<HTMLButtonElement>;
-  className?: ButtonHTMLAttributes<HTMLButtonElement>;
+  className?: string;
+  icon?: React.ReactNode;
 }
 
 export default function Button({
@@ -12,13 +13,27 @@ export default function Button({
   type = "primary",
   props,
   className,
+  icon,
 }: IButton) {
-  return (
-    <button
-      className={`h-fit w-fit rounded bg-yellow p-2 transition-colors hover:bg-yellow-dark ${className}`}
-      {...props}
-    >
-      {label}
-    </button>
-  );
+  if (type == "primary")
+    return (
+      <button
+        className={`text-base-white h-fit min-h-8 w-fit rounded-md bg-yellow p-2 font-roboto text-sm uppercase transition-colors hover:bg-yellow-dark ${className}`}
+        {...props}
+      >
+        {label}
+      </button>
+    );
+  else
+    return (
+      <button
+        className={`h-fit min-h-8 w-fit rounded-md bg-base-button p-2 transition-colors hover:bg-base-hover ${className} flex items-center gap-1`}
+        {...props}
+      >
+        {icon}
+        <span className="font-roboto text-sm uppercase text-base-subtitle">
+          {label}
+        </span>
+      </button>
+    );
 }
