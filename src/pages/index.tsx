36c3138af5a46d02a7acfe50336cb3b0b231890c
lucasptcastro <lucasptcastro@gmail.com>
2024-02-18 10:39:31 -0300

    ✨ feat: added COFFEES data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,117 +1,14 @@
 import Image from "next/image";
 import Layout from "./layout";
 import Loader from "@/components/Loader";
-import Catalog, { ICatalogo } from "@/components/Catalog";
+import Catalog from "@/components/Catalog";
 import { Coffee, Package, ShoppingCart, Timer } from "@phosphor-icons/react";
 import { useEffect, useState } from "react";
 
+import { COFFEES } from "@/utils/data/products";
+
 export default function Home() {
   const [loading, setLoading] = useState(true);
-  const coffees: Array<ICatalogo> = [
-    {
-      coffee_image: "/coffees/expresso.svg",
-      tags: ["Tradicional"],
-      title: "Expresso Tradicional",
-      description: "O tradicional café feito com água quente e grãos moídos",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/americano.svg",
-      tags: ["Tradicional"],
-      title: "Expresso Americano",
-      description: "Expresso diluído, menos intenso que o tradicional",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/expresso_cremoso.svg",
-      tags: ["Tradicional"],
-      title: "Expresso Cremoso",
-      description: "Café expresso tradicional com espuma cremosa",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/cafe_gelado.svg",
-      tags: ["Tradicional", "Gelado"],
-      title: "Expresso Gelado",
-      description: "Bebida preparada com café expresso e cubos de gelo",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/cafe_com_leite.svg",
-      tags: ["Tradicional", "Com leite"],
-      title: "Café com Leite",
-      description: "Meio a meio de expresso tradicional com leite vaporizado",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/latte.svg",
-      tags: ["Tradicional", "Com leite"],
-      title: "Latte",
-      description:
-        "Uma dose de café expresso com o dobro de leite e espuma cremosa",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/capuccino.svg",
-      tags: ["Tradicional", "Com leite"],
-      title: "Capuccino",
-      description:
-        "Bebida com canela feita de doses iguais de café, leite e espuma",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/macchiato.svg",
-      tags: ["Tradicional", "Com leite"],
-      title: "Macchiato",
-      description:
-        "Café expresso misturado com um pouco de leite quente e espuma",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/mochaccino.svg",
-      tags: ["Tradicional", "Com leite"],
-      title: "Mocaccino",
-      description: "Café expresso com calda de chocolate, pouco leite e espuma",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/chocolate_quente.svg",
-      tags: ["Tradicional", "Com leite"],
-      title: "Chocolate Quente",
-      description:
-        "Bebida feita com chocolate dissolvido no leite quente e café",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/cubano.svg",
-      tags: ["Especial", "Alcoólico", "Gelado"],
-      title: "Cubano",
-      description:
-        "Drink gelado de café expresso com rum, creme de leite e hortelã",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/havaiano.svg",
-      tags: ["Especial"],
-      title: "Havaiano",
-      description: "Bebida adocicada preparada com café e leite de coco",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/arabe.svg",
-      tags: ["Especial"],
-      title: "Árabe",
-      description: "Bebida preparada com grãos de café árabe e especiarias",
-      price: 9.9,
-    },
-    {
-      coffee_image: "/coffees/irlandes.svg",
-      tags: ["Especial", "Alcoólico"],
-      title: "Irlandês",
-      description: "Bebida a base de café, uísque irlandês, açúcar e chantilly",
-      price: 9.9,
-    },
-  ];
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 1500);
@@ -147,7 +44,7 @@ export default function Home() {
                 <ShoppingCart
                   size={16}
                   weight="fill"
-                  className="text-background"
+                  className="text-base-background"
                 />
               </div>
               <span className="font-roboto text-sm text-base-text xl:text-base">
@@ -156,7 +53,11 @@ export default function Home() {
             </div>
             <div className="flex items-center gap-3">
               <div className="flex h-7 w-7 items-center justify-center rounded-full bg-base-text p-2 xl:h-8 xl:w-8">
-                <Package size={16} weight="fill" className="text-background" />
+                <Package
+                  size={16}
+                  weight="fill"
+                  className="text-base-background"
+                />
               </div>
               <span className="font-roboto text-sm text-base-text xl:text-base">
                 Embalagem mantém o café intacto
@@ -164,7 +65,11 @@ export default function Home() {
             </div>
             <div className="flex items-center gap-3">
               <div className="flex h-7 w-7 items-center justify-center rounded-full bg-yellow p-2 xl:h-8 xl:w-8">
-                <Timer size={16} weight="fill" className="text-background" />
+                <Timer
+                  size={16}
+                  weight="fill"
+                  className="text-base-background"
+                />
               </div>
               <span className="font-roboto text-sm text-base-text xl:text-base">
                 Entrega rápida e rastreada
@@ -172,7 +77,11 @@ export default function Home() {
             </div>
             <div className="flex items-center gap-3">
               <div className="flex h-7 w-7 items-center justify-center rounded-full bg-purple p-2 xl:h-8 xl:w-8">
-                <Coffee size={16} weight="fill" className="text-background" />
+                <Coffee
+                  size={16}
+                  weight="fill"
+                  className="text-base-background"
+                />
               </div>
               <span className="font-roboto text-sm text-base-text xl:text-base">
                 O café chega fresquinho até você
@@ -198,14 +107,15 @@ export default function Home() {
 
         {/* Catalog */}
         <div className="flex w-full flex-col items-center justify-center space-y-10 py-10 md:grid md:grid-cols-2 md:gap-5 lg:justify-items-center xl:grid-cols-3 3xl:grid-cols-4">
-          {coffees.map((coffee) => (
+          {COFFEES.map((coffee, index) => (
             <Catalog
+              id={coffee.id}
               coffee_image={coffee.coffee_image}
               tags={coffee.tags}
               title={coffee.title}
               description={coffee.description}
               price={coffee.price}
-              key={coffee.title}
+              key={index}
             />
           ))}
         </div>
