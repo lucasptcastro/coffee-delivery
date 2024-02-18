c5c6fb9db1cd69776dde6bc76399b9ee21f197b3
lucasptcastro <lucasptcastro@gmail.com>
2024-02-18 10:37:07 -0300

    ✨ feat: added CartContextProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import { Roboto } from "next/font/google";
 import { Baloo_2 } from "next/font/google";
+import { CartContextProvider } from "../contexts/Cart";
 
 const roboto = Roboto({
   weight: ["400", "900"],
@@ -15,8 +16,10 @@ const baloo2 = Baloo_2({
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${roboto.className} ${baloo2.className}`}>
-      <Component {...pageProps} />
-    </main>
+    <CartContextProvider>
+      <main className={`${roboto.className} ${baloo2.className}`}>
+        <Component {...pageProps} />
+      </main>
+    </CartContextProvider>
   );
 }
