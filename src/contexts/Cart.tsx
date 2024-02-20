0391034b179450cbcb2f15727efc1acafde4d62f
lucasptcastro <lucasptcastro@gmail.com>
2024-02-19 22:03:30 -0300

    ✨ feat: created changeProductQuantity function

--- src/contexts/Cart.tsx
+++ src/contexts/Cart.tsx
@@ -6,6 +6,10 @@ export type CartContextDataProps = {
 
   addProductToCart: (product: ICartProduct) => void;
   removeProductFromCart: (product: ICartProduct) => void;
+  changeProductQuantity: (
+    product: ICartProduct,
+    operation: "plus" | "minus",
+  ) => void;
 };
 
 type CartContextProviderProps = {
@@ -47,9 +51,53 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
     }
   }
 
+  function changeProductQuantity(
+    product: ICartProduct,
+    operation: "plus" | "minus",
+  ) {
+    // verifica se o produto está no carrinho
+    const productIndex = cartProducts?.findIndex(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+
+    if (cartProducts && productIndex! >= 0) {
+      // se estiver no carrinho
+      if (operation === "plus") {
+        const productsArray: ICartProduct[] = cartProducts;
+        cartProducts[productIndex!].quantity =
+          cartProducts[productIndex!].quantity! + 1;
+
+        setCartProducts([...productsArray]);
+      } else {
+        if (cartProducts[productIndex!].quantity! > 1) {
+          const productsArray: ICartProduct[] = cartProducts;
+          cartProducts[productIndex!].quantity =
+            cartProducts[productIndex!].quantity! - 1;
+
+          setCartProducts([...productsArray]);
+        } else {
+          removeProductFromCart(product);
+        }
+      }
+    } else {
+      addProductToCart({
+        id: product.id,
+        imagePath: product.imagePath,
+        price: product.price,
+        product: product.product,
+        quantity: 1,
+      });
+    }
+  }
+
   return (
     <CartContext.Provider
-      value={{ cartProducts, addProductToCart, removeProductFromCart }}
+      value={{
+        cartProducts,
+        addProductToCart,
+        removeProductFromCart,
+        changeProductQuantity,
+      }}
     >
       {children}
     </CartContext.Provider>
